2c0eabc1246ed8dfd237d0e5546effb0e6d9e86f
Om Gupta <om.gupta@teknopoint.in>
2024-08-29 19:35:03 +0530

    Carousel Configurable added

---
+++ blocks/carousel/carousel-config.js
@@ -0,0 +1,32 @@
+const heroCarousel = {
+  loop: true,
+  slidesPerView: 1,
+  grabCursor: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    // nextEl: swiperButtonNext,
+    // prevEl: swiperButtonPrev,
+  },
+  pagination: {
+    // el: swiperPagination,
+    clickable: true,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+    },
+    780: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 1,
+    },
+    1199: {
+      slidesPerView: 1,
+    },
+  },
+};
+export default { 'hero-carousel': heroCarousel };
