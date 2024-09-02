aa10c94c82c32bdb00b5c10ed08e06e51358d87e
pashtepooja18 <poojapashte18@gmail.com>
2024-09-02 12:50:55 +0530

    turbo charge your investments carousel css added

--- blocks/carousel/carousel-config.js
+++ blocks/carousel/carousel-config.js
@@ -66,10 +66,10 @@ const oneThirdCarousel = {
   loop: true,
   slidesPerView: 3,
   grabCursor: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: false,
+  // },
   navigation: {
     // nextEl: swiperButtonNext,
     // prevEl: swiperButtonPrev,
@@ -83,13 +83,15 @@ const oneThirdCarousel = {
       slidesPerView: 1,
     },
     780: {
-      slidesPerView: 2,
+      slidesPerView: 1,
     },
     992: {
-      slidesPerView: 3,
+      slidesPerView: 1,
     },
-    1199: {
+    1025: {
       slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 20,
     },
   },
 };
